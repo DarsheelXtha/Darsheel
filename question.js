bf150bb601abd443c562ebc7f79534ff2fab0243
Orish <hsirov321@gmail.com>
2020-09-15 14:25:14 +0545

    Changing

--- question.js
+++ question.js
@@ -1,23 +1,23 @@
-
 const quiz = [
     {
         q:' What is the first name of former Russian President the Mr.Putin?',
         options:['Albert','Michael','Uri','Viadmir'],
-        answer: 3
+        answer: 'Uri'
     },
     {
         q:'3+4=7',
         options:['true','false'],
-        answer:1
+        answer:true
     },
     {
         q:'what is 22+6?',
-        options:['99','56','16','28'] ,
-        answer:3
+        options:[99,56,16,28] ,
+        answer:16
     },
     {
         q:'what is color of banana?',
         options:['red','yellow','white','blue'] ,
-        answer:1
+        answer:'yellow'
     },
 ]
+let insName ='Batash ko com';
