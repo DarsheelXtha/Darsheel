0604a3ec5063146fd7063da9fec32339694c348f
Orish <hsirov321@gmail.com>
2020-09-15 15:42:58 +0545

    error solved

--- app.js
+++ app.js
@@ -1,2 +1,37 @@
+let qweLength = quiz.length;
 document.querySelector('#instutin_Name').innerHTML=insName;
-document.querySelector('.qwe-no').innerHTML = quiz.length
+document.querySelector('.qwe-no').innerHTML = qweLength;
+let qweTitel = document.querySelector('#qwe');
+let qweOptions = document.querySelector("#options");
+var qweNo= 0
+
+let chekAns=(qweNo,key)=>{
+console.log(qweNo, key)
+if(quiz[qweNo].answer === key){
+    alert("right");
+}else{
+    alert("w")
+}
+
+}
+let reqQue=(qweNo)=>{
+    qweTitel.innerHTML='';
+    qweOptions.innerHTML ='';
+    qweTitel.innerHTML=quiz[qweNo].q;
+    quiz[qweNo].options.map((opt,key)=>{
+        qweOptions.innerHTML +=`<input type="radio" name="${qweNo}" onchange='chekAns(${qweNo},${key})' value="${opt}" id='${opt}${key}'><label class="pointer" for="${opt}${key}">${opt}</label> `;
+    })
+}
+let result = ()=>{
+    alert();
+}
+let nextQwe=()=>{
+    qweNo < qweLength-1 ? qweNo+=1: result(); ;
+    reqQue(qweNo);
+}
+let reset=()=>{
+    qweNo=0
+    reqQue(qweNo);
+}
+
+reqQue(qweNo);
--- question.js
+++ question.js
@@ -2,22 +2,22 @@ const quiz = [
     {
         q:' What is the first name of former Russian President the Mr.Putin?',
         options:['Albert','Michael','Uri','Viadmir'],
-        answer: 'Uri'
+        answer: 2
     },
     {
         q:'3+4=7',
         options:['true','false'],
-        answer:true
+        answer:0
     },
     {
         q:'what is 22+6?',
         options:[99,56,16,28] ,
-        answer:16
+        answer:3
     },
     {
         q:'what is color of banana?',
         options:['red','yellow','white','blue'] ,
-        answer:'yellow'
+        answer:1
     },
 ]
 let insName ='Batash ko com';
